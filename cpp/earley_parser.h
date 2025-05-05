4ffdd47c4f10b7a4b29d56a109b1eca8506c01cf
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-05-05 10:24:13 +0800

    test:test threshold.

--- cpp/earley_parser.h
+++ cpp/earley_parser.h
@@ -149,7 +149,7 @@ class RepeatDetector {
   int size_ = 0;
 
  public:
-  RepeatDetector(const int& transition_threshold = 100)
+  RepeatDetector(const int& transition_threshold = 200)
       : transition_threshold_(transition_threshold),
         visited_states_(std::vector<ParserState>()),
         size_(0) {
