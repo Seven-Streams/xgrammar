2df3f2551214d45b90ef302bc8b688e33a6bac0d
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-05-21 20:04:23 +0800

    format:fix the format of the comments.

--- cpp/earley_parser.h
+++ cpp/earley_parser.h
@@ -20,23 +20,23 @@
 
 namespace xgrammar {
 
-/* \brief The state of the Earley parser.
-  In the implementation, a rule can only be a kchoices or a ktagdispatch.
-  A kchoices rule must be composed of some ksequence rules, or a kemptyrule.
-  In the ksequence, every element in the sequence must be a kbytestring, a
-  kcharacterclass, a kcharacterclassstar, or a rule reference.
-
-  -rule_id: The id of the rule.
-  -sequence_id: The id of the sequence in the rule.
-  -element_id: The id of the element in the sequence, or the id of the node in
-  the tag dispatch fsm.
-  -parent_pos: The id of the parent node in the Earley parser. i.e. the rule
-  is predicted from the k-th character.
-  -sub_element_id: The id of the sub element in the current element, i.e.:
-    - kbytestring: the id of the byte in the string.
-    - kcharacterclass: How many bytes are left to be read in the utf8 character.
-    - kcharacterclassstar: How many bytes are left to be read in the utf8 character.
-*/
+/*! \brief The state of the Earley parser.
+ * In the implementation, a rule can only be a kchoices or a ktagdispatch.
+ * A kchoices rule must be composed of some ksequence rules, or a kemptyrule.
+ * In the ksequence, every element in the sequence must be a kbytestring, a
+ * kcharacterclass, a kcharacterclassstar, or a rule reference.
+ *
+ * -rule_id: The id of the rule.
+ * -sequence_id: The id of the sequence in the rule.
+ * -element_id: The id of the element in the sequence, or the id of the node in
+ * the tag dispatch fsm.
+ * -parent_pos: The id of the parent node in the Earley parser. i.e. the rule
+ * is predicted from the k-th character.
+ * -sub_element_id: The id of the sub element in the current element, i.e.:
+ *   - kbytestring: the id of the byte in the string.
+ *   - kcharacterclass: How many bytes are left to be read in the utf8 character.
+ *   - kcharacterclassstar: How many bytes are left to be read in the utf8 character.
+ */
 struct ParserState {
   constexpr ParserState() = default;
 
@@ -58,7 +58,8 @@ struct ParserState {
         sub_element_id(sub_element_id) {}
 
   /*! \brief A sequence_id value of kUnexpandedRuleStartSequenceId means a rule hasn't been
-   * expanded.*/
+   * expanded.
+   */
   static constexpr int32_t kUnexpandedRuleStartSequenceId = 128000;
 
   /*! \brief A parent_id value of kNoParent means this ParserState is the root of the parsing stack.
@@ -75,7 +76,8 @@ struct ParserState {
   int32_t sequence_id = -1;
 
   /*! \brief Which element of the choice sequence is to be visited. When the current sequence is
-   * a tag dispatch rule, this element id the currently visited node. */
+   * a tag dispatch rule, this element id the currently visited node.
+   */
   int32_t element_id = -1;
 
   /*! \brief The position of the state, i.e. from which position, the rule starts.*/
@@ -104,7 +106,7 @@ struct ParserState {
 
 /*
   When getting the mask of the state, we don't need to consider the parent_pos.
-*/
+ */
 class StateHash {
  public:
   size_t operator()(const ParserState& state) const {
@@ -115,7 +117,7 @@ class StateHash {
 /*
   When matching the state, we need to consider the parent_pos, since if two states
   don't have the same parent_pos, they are not the same state.
-*/
+ */
 class StateEqual {
  public:
   bool operator()(const ParserState& lhs, const ParserState& rhs) const {
@@ -151,12 +153,14 @@ class RepeatDetector {
     visited_vector_.resize(transition_threshold_);
   }
 
-  /*! \brief Check if the elemenet is visited.
-      \return True if visited, false otherwise.*/
+  /*! \brief Check if the element is visited.
+   * \return True if visited, false otherwise.
+   */
   bool IsVisited(const ParserState& state) const;
 
   /*! \brief Add the state into the visited states.
-      \param state The state to be added.*/
+   * \param state The state to be added.
+   */
   void Insert(const ParserState& state);
 
   /*! \brief Reset the detector.*/
@@ -165,15 +169,15 @@ class RepeatDetector {
 
 class EarleyParser {
   /*!
-    \brief Here is an article about Earley
-    Parser.https://en.wikipedia.org/wiki/Earley_parser#Pseudocode We divide the parser states into
-    three categories:
-    - Scanable (which will be stored in scanable_state_history_).
-    - Predictable(If it predict a new rule successfully, then it will be stored in
-    rule_id_to_completeable_states).
-    - Completeable(which can perform a completion operation).
-    One state can be in multiple categories, and thus can be stored in multiple places.
-    */
+   * \brief Here is an article about Earley Parser.
+   * https://en.wikipedia.org/wiki/Earley_parser#Pseudocode
+   * We divide the parser states into three categories:
+   * - Scanable (which will be stored in scanable_state_history_).
+   * - Predictable(If it predict a new rule successfully, then it will be stored in
+   * rule_id_to_completeable_states).
+   * - Completeable(which can perform a completion operation).
+   * One state can be in multiple categories, and thus can be stored in multiple places.
+   */
  protected:
   /*! \brief The grammar to be parsed. */
   Grammar grammar_;
@@ -185,17 +189,19 @@ class EarleyParser {
   std::vector<bool> can_accept_stop_token_;
 
   /*! \brief rule_id_to_completeable_states[i][j] is the i pos j rule_id states. Earley
-      parser needs it to complete. */
+   * parser needs it to complete.
+   */
   std::vector<std::multimap<int32_t, ParserState>> rule_id_to_completeable_states_;
 
   /*!
-      \brief The states history. state_stack[i] is a vector storing the states after accepting the
-     input[i-1].
+   * \brief The states history. state_stack[i] is a vector storing the states after accepting the
+   * input[i-1].
    */
   CSRArray<ParserState> scanable_state_history_;
 
   /*! \brief A temperate vector only used in Advance, used to add states in the
-   * scanable_state_history. */
+   * scanable_state_history.
+   */
   std::vector<ParserState> tmp_states_to_be_added_;
 
   /*! \brief It's the processing queue of the earley parser.*/
@@ -205,53 +211,51 @@ class EarleyParser {
   RepeatDetector tmp_states_visited_in_queue_;
 
   /*!
-  \brief Check if the state has been added into the queue.
-  \param state The state to check.
-  \return True if in the vector, false otherwise.
-*/
+   * \brief Check if the state has been added into the queue.
+   * \param state The state to check.
+   * \return True if in the vector, false otherwise.
+   */
   bool IsStateVisitedInQueue(const ParserState& state) const {
     return tmp_states_visited_in_queue_.IsVisited(state);
   }
 
-  /*!
-    \brief The scanning operation of the Earley parser.
-  */
+  /*! \brief The scanning operation of the Earley parser. */
   void Scan(const ParserState& state, const uint8_t& ch);
 
   /*!
-      \brief The completion operation of the Earley parser.
-      \details The reason is that if the state can't be scanned, then
-      add it into the next states is useless. Moreover, the end
-      of the grammar is used to check if the grammar is completed,
-      so it should be added into the next states.
-  */
+   * \brief The completion operation of the Earley parser.
+   * \details The reason is that if the state can't be scanned, then
+   * add it into the next states is useless. Moreover, the end
+   * of the grammar is used to check if the grammar is completed,
+   * so it should be added into the next states.
+   */
   void Complete(const ParserState& state, const Grammar::Impl::RuleExpr& rule_expr);
 
   /*!
-      \brief The prediction operation of the Earley parser.
-      \return Fitst: If the state scanable, or the state is the end of the grammar,
-      then return true, otherwise return false.
-      \return Second: If the state is completable, then return true, otherwise return false.
-  */
+   * \brief The prediction operation of the Earley parser.
+   * \return First: If the state scanable, or the state is the end of the grammar,
+   * then return true, otherwise return false.
+   * \return Second: If the state is completable, then return true, otherwise return false.
+   */
   std::pair<bool, bool> Predict(const ParserState& state, Grammar::Impl::RuleExpr* rule_expr);
 
   /*!
-    \brief Handle the unexpanded rule, used for pushing initial state.
-    \param state The state to be handled.
-    \return True if the rule is unexpanded, false otherwise.
-  */
+   * \brief Handle the unexpanded rule, used for pushing initial state.
+   * \param state The state to be handled.
+   * \return True if the rule is unexpanded, false otherwise.
+   */
   bool ExpandAndEnqueueUnexpandedState(const ParserState& state);
 
   /*!
-    \brief Expand the rule, used for RuleRef and kTagDispatch.
-    \param state The state to be expanded, which is the parent state.
-    The type of the state is kTagDispatch or kSequence. Moreover, the
-    element of the sequence should be a rule reference; the node in
-    the kTagDispatch should be an end node.
-    \param rule_expr The rule expression to be expanded.
-    \param sub_rule_expr The sub rule expression to be expanded, especially
-    when the rule is a kSequence, and the sub rule is a kRuleRef.
-  */
+   * \brief Expand the rule, used for RuleRef and kTagDispatch.
+   * \param state The state to be expanded, which is the parent state.
+   * The type of the state is kTagDispatch or kSequence. Moreover, the
+   * element of the sequence should be a rule reference; the node in
+   * the kTagDispatch should be an end node.
+   * \param rule_expr The rule expression to be expanded.
+   * \param sub_rule_expr The sub rule expression to be expanded, especially
+   * when the rule is a kSequence, and the sub rule is a kRuleRef.
+   */
   void ExpandNextRuleRefElement(
       const ParserState& state,
       const Grammar::Impl::RuleExpr& rule_expr,
@@ -259,54 +263,54 @@ class EarleyParser {
   );
 
   /*!
-    \brief Advance the parser to the next state, with the sub sequence is kCharacterClass.
-    \param state The state to be advanced.
-    \param ch The character to be advanced.
-    \param sub_sequence The sub sequence to be checked.
-    \return The next state, Invalid state if the character is not accepted.
-  */
+   * \brief Advance the parser to the next state, with the sub sequence is kCharacterClass.
+   * \param state The state to be advanced.
+   * \param ch The character to be advanced.
+   * \param sub_sequence The sub sequence to be checked.
+   * \return The next state, Invalid state if the character is not accepted.
+   */
   void AdvanceCharacterClass(
       const ParserState& state, const uint8_t& ch, const Grammar::Impl::RuleExpr& sub_sequence
   );
 
   /*!
-    \brief Advance the parser to the next state, with the sub sequence is kByteString.
-    \param state The state to be advanced.
-    \param ch The character to be advanced.
-    \param sub_sequence The sub sequence to be checked.
-    \return The next state, Invalid state if the character is not accepted.
-  */
+   * \brief Advance the parser to the next state, with the sub sequence is kByteString.
+   * \param state The state to be advanced.
+   * \param ch The character to be advanced.
+   * \param sub_sequence The sub sequence to be checked.
+   * \return The next state, Invalid state if the character is not accepted.
+   */
   void AdvanceByteString(
       const ParserState& state, const uint8_t& ch, const Grammar::Impl::RuleExpr& sub_sequence
   );
 
   /*!
-    \brief Advance the parser to the next state, with the sub sequence is kCharacterClassStar.
-    \param state The state to be advanced.
-    \param ch The character to be advanced.
-    \param sub_sequence The sub sequence to be checked.
-    \return The next state, Invalid state if the character is not accepted.
-  */
+   * \brief Advance the parser to the next state, with the sub sequence is kCharacterClassStar.
+   * \param state The state to be advanced.
+   * \param ch The character to be advanced.
+   * \param sub_sequence The sub sequence to be checked.
+   * \return The next state, Invalid state if the character is not accepted.
+   */
   void AdvanceCharacterClassStar(
       const ParserState& state, const uint8_t& ch, const Grammar::Impl::RuleExpr& sub_sequence
   );
 
   /*!
-    \brief Advance the parser to the next state, with the sequence is kTagDispatch.
-    \param state The state to be advanced.
-    \param ch The character to be advanced.
-    \param cur_sequence The sequence of the current state.
-    \return The next state, Invalid state if the character is not accepted.
-  */
+   * \brief Advance the parser to the next state, with the sequence is kTagDispatch.
+   * \param state The state to be advanced.
+   * \param ch The character to be advanced.
+   * \param cur_sequence The sequence of the current state.
+   * \return The next state, Invalid state if the character is not accepted.
+   */
   void AdvanceTagDispatch(
       const ParserState& state, const uint8_t& ch, const Grammar::Impl::RuleExpr& cur_sequence
   );
 
   /*!
-    \brief Enqueue the state into the queue.
-    \param state The state to be enqueued.
-    \details The state is enqueued if it is not visited in the queue.
-  */
+   * \brief Enqueue the state into the queue.
+   * \param state The state to be enqueued.
+   * \details The state is enqueued if it is not visited in the queue.
+   */
   void Enque(const ParserState& state) {
     if (!IsStateVisitedInQueue(state)) {
       tmp_process_state_queue_.push(state);
@@ -323,47 +327,47 @@ class EarleyParser {
 
  public:
   /*!
-   \brief Constructor of the Earley parser.
-   \param grammar The grammar to be parsed.
-   \param initial_state The initial state to be pushed into the parser.
- */
+   * \brief Constructor of the Earley parser.
+   * \param grammar The grammar to be parsed.
+   * \param initial_state The initial state to be pushed into the parser.
+   */
   EarleyParser(
       const Grammar& grammar, const ParserState& initial_state, const bool& need_expand = true
   );
 
   /*!
-    \brief From the current states, advance to the next state.
-    \param ch The character to be advanced.
-    \return True if the character is accepted, false otherwise.
-    \note If the character isn't accepted, then the states won't be changed.
-  */
+   * \brief From the current states, advance to the next state.
+   * \param ch The character to be advanced.
+   * \return True if the character is accepted, false otherwise.
+   * \note If the character isn't accepted, then the states won't be changed.
+   */
   bool Advance(const uint8_t& ch);
 
   /*!
-    \brief Remove the newly added states.
-    \param count The number of states to be removed.
-  */
+   * \brief Remove the newly added states.
+   * \param count The number of states to be removed.
+   */
   void PopLastStates(int32_t count = 1);
 
   /*!
-    \brief Check whether any of the multiple states stored in the parser has already completed.
-    \note Since the parser contains multiple parallel states, some may have already completed, while
-    others might still be able to accept more characters.
-    \return True if the root rule is completed, false otherwise.
-  */
+   * \brief Check whether any of the multiple states stored in the parser has already completed.
+   * \note Since the parser contains multiple parallel states, some may have already completed,
+   * while others might still be able to accept more characters.
+   * \return True if the root rule is completed, false otherwise.
+   */
   bool IsCompleted() const;
 
   /*!
-    \brief Push the initial state into the Earley parser.
-    \param state The initial state to be pushed.
-  */
+   * \brief Push the initial state into the Earley parser.
+   * \param state The initial state to be pushed.
+   */
   void PushStateAndExpand(const ParserState& state);
 
   /*!
-    \brief Reset the parser.
-    \note This function is used to reset the parser, and initialize the
-    parser with the root rule.
-  */
+   * \brief Reset the parser.
+   * \note This function is used to reset the parser, and initialize the
+   * parser with the root rule.
+   */
   void Reset();
 };
 
