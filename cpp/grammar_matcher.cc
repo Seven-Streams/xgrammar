8cb07087c7e6e1175137134e0bedecc400ae8dcb
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-05-25 16:50:08 +0800

    optim:optim the matcher.

--- cpp/grammar_matcher.cc
+++ cpp/grammar_matcher.cc
@@ -498,12 +498,7 @@ bool GrammarMatcher::Impl::FillNextTokenBitmask(
       CheckAndGetBitmaskPtr(*next_token_bitmask, tokenizer_info_.GetVocabSize(), index);
   const auto& sorted_decoded_vocab = tokenizer_info_.GetSortedDecodedVocab();
   const auto& adaptive_token_mask_cache = compiled_grammar_->adaptive_token_mask_cache;
-  std::vector<ParserState> latest_states;
-  for (const auto& ParserState : scanable_state_history_[scanable_state_history_.Size() - 1]) {
-    if (ParserState.sequence_id != ParserState::kUnexpandedRuleStartSequenceId) {
-      latest_states.push_back(ParserState);
-    }
-  }
+  const auto& latest_states = scanable_state_history_[scanable_state_history_.Size() - 1];
 
   // We check all the latest states of the earley parser, and check all the masks of the leaf
   // states. The final accepted token set is the union of the accepted token sets of all leaf
