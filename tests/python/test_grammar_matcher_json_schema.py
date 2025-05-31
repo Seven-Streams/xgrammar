93cfb4f45e8359ee0a54bb74a47cc1766d473f6a
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-05-31 17:12:38 +0800

    test:add the tests.

--- tests/python/test_grammar_matcher_json_schema.py
+++ tests/python/test_grammar_matcher_json_schema.py
@@ -394,85 +394,17 @@ def test_implicit_left_recursion_schema():
     json_schema = {
         "$schema": "http://json-schema.org/draft-04/schema#",
         "type": "object",
-        "required": [
-            "location",
-            "country",
-            "city",
-            "parameter",
-            "unit",
-            "value",
-            "date",
-            "sourceName",
-            "sourceType",
-            "mobile",
-        ],
         "properties": {
-            "location": {"type": "string", "minLength": 1},
-            "parameter": {
+            "url": {
                 "type": "string",
-                "enum": ["pm25", "pm10", "no2", "so2", "o3", "co", "bc"],
-            },
-            "unit": {"type": "string", "enum": ["ug/m^3", "ppm"]},
-            "averagingPeriod": {
-                "type": "object",
-                "required": ["value", "unit"],
-                "additionalProperties": False,
-                "properties": {
-                    "value": {"type": "number"},
-                    "unit": {"type": "string", "enum": ["hours"]},
-                },
-            },
-            "attribution": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "required": ["name"],
-                    "additionalProperties": False,
-                    "properties": {
-                        "name": {"type": "string", "minLength": 1},
-                        "url": {
-                            "type": "string",
-                            "pattern": "^(https?://)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([/\\w \\.-]*)*/?",
-                        },
-                    },
-                },
-            },
-            "coordinates": {
-                "type": "object",
-                "required": ["latitude", "longitude"],
-                "additionalProperties": False,
-                "properties": {
-                    "latitude": {"type": "number", "minimum": -90, "maximum": 90},
-                    "longitude": {"type": "number", "minimum": -180, "maximum": 180},
-                },
-            },
-            "value": {"type": "number"},
-            "date": {
-                "type": "object",
-                "additionalProperties": False,
-                "required": ["utc", "local"],
-                "properties": {
-                    "utc": {"type": "string"},
-                    "local": {
-                        "type": "string",
-                        "pattern": "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\+|-)\\d{2}:\\d{2}",
-                    },
-                },
-            },
-            "sourceName": {"type": "string", "minLength": 1},
-            "sourceType": {"type": "string", "enum": ["government", "research", "other"]},
-            "mobile": {"type": "boolean"},
-            "city": {"type": "string", "minLength": 1},
-            "country": {"type": "string", "maxLength": 2, "minLength": 2},
+                "pattern": "^(https?://)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([/\\w \\.-]*)*/?",
+            }
         },
     }
-
     tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer, vocab_size=config.vocab_size)
     grammar_compiler = xgr.GrammarCompiler(tokenizer_info)
-    time_start = time.monotonic_ns()
     _ = grammar_compiler.compile_json_schema(schema=dumps(json_schema))
-    time_end = time.monotonic_ns()
-    print(f"Time for preprocessing: {(time_end - time_start) / 1e3} us")
+    print(_.grammar)
 
 
 if __name__ == "__main__":
