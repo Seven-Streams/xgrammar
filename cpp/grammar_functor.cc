062aa3b1bcc76775ef66b444c6bbb3cac3054960
Yuchuan <yuchuan.7streams@gmail.com>
2025-10-09 16:27:38 +0800

    no minimize dfa.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- cpp/grammar_functor.cc
+++ cpp/grammar_functor.cc
@@ -1879,7 +1879,7 @@ std::optional<FSMWithStartEnd> GrammarFSMBuilderImpl::Choices(
 
   auto result = FSMWithStartEnd::Union(fsm_list);
   result = result.SimplifyEpsilon();
-  // result = result.MergeEquivalentSuccessors();
+  result = result.MergeEquivalentSuccessors();
   // auto result_raw = result.MinimizeDFA();
   // if (result_raw.IsOk()) {
   //   result = std::move(result_raw).Unwrap();
