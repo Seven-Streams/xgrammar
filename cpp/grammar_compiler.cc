9bd944f06833ff1a5a8026460831b7b7edbec92d
Yuchuan <yuchuan.7streams@gmail.com>
2026-01-31 11:17:53 +0800

    test.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- cpp/grammar_compiler.cc
+++ cpp/grammar_compiler.cc
@@ -788,19 +788,19 @@ AdaptiveTokenMask GrammarMatcherForTokenMaskCache::GetAdaptiveTokenMask(bool is_
       }
     }
     // XGRAMMAR_DCHECK(new_state_id.has_value());
-    // const auto& fsm = grammar_->per_rule_fsms[init_rule_id_].value();
-    // if (lookahead_hash.has_value()) {
-    //   crossing_cache = rule_level_cache_->GetCache(
-    //       HashCombine(fsm_hash.value(), lookahead_hash.value(), is_exact_lookahead),
-    //       new_state_id.value(),
-    //       fsm.NumStates(),
-    //       fsm.GetNumEdges()
-    //   );
-    //   if (crossing_cache.has_value()) {
-    //     // A perfect match.
-    //     return crossing_cache.value();
-    //   }
-    // }
+    const auto& fsm = grammar_->per_rule_fsms[init_rule_id_].value();
+    if (lookahead_hash.has_value()) {
+      crossing_cache = rule_level_cache_->GetCache(
+          HashCombine(fsm_hash.value(), lookahead_hash.value(), is_exact_lookahead),
+          new_state_id.value(),
+          fsm.NumStates(),
+          fsm.GetNumEdges()
+      );
+      if (crossing_cache.has_value()) {
+        // A perfect match.
+        return crossing_cache.value();
+      }
+    }
     // crossing_cache = rule_level_cache_->GetCache(
     //     fsm_hash.value(), new_state_id.value(), fsm.NumStates(), fsm.GetNumEdges()
     // );
