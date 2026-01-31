cbc74e2cb83bafd3f46e1bff97b6305b8f9dc887
Yuchuan <yuchuan.7streams@gmail.com>
2026-01-31 13:13:56 +0800

    test.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- cpp/grammar_compiler.cc
+++ cpp/grammar_compiler.cc
@@ -783,18 +783,18 @@ AdaptiveTokenMask GrammarMatcherForTokenMaskCache::GetAdaptiveTokenMask(bool is_
     new_state_id = get_new_state_id->second;
     const auto& fsm = grammar_->per_rule_fsms[init_rule_id_].value();
     if (lookahead_hash.has_value()) {
-      auto crossing_cache = rule_level_cache_->GetCache(
+      rule_level_cache_->GetCache(
           HashCombine(fsm_hash.value(), lookahead_hash.value(), is_exact_lookahead),
           new_state_id.value(),
           fsm.NumStates(),
           fsm.GetNumEdges()
       );
-      if (crossing_cache.has_value()) {
-        // A perfect match.
-        return crossing_cache.value();
-      }
+      // if (crossing_cache.has_value()) {
+      //   // A perfect match.
+      //   return crossing_cache.value();
+      // }
     }
-    auto crossing_cache = rule_level_cache_->GetCache(
+    rule_level_cache_->GetCache(
         fsm_hash.value(), new_state_id.value(), fsm.NumStates(), fsm.GetNumEdges()
     );
     // // If the rule doesn't have a lookahead, then it is exactly the same fsm.
